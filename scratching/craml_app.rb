05b3a29ae7923fcfeb2e797b41354ff015f0b23d
Zachary Abresch <zachary.abresch@gmail.com>
2015-04-30 21:31:36 -0500

    Some work for MongoDB [Unfinished]
    I got down some rabbit holes in trying to get persistence.
    
    Think it'll be smarter to move onto using Rails.

--- scratching/craml_app.rb
+++ scratching/craml_app.rb
@@ -1,15 +1,36 @@
-require 'sinatra'
-require 'json'
-require_relative './craml.rb'
 
-use Rack::Logger
+require 'rubygems'
+require 'sinatra'
+require 'mongo'
+require 'json'
+require 'json/ext' # required for .to_json
+
+Bundler.require(:default)
+
+include Mongo
+
+# require_relative './craml.rb'
+
+# use Rack::Logger
+
+configure do
+  conn = MongoClient.new("localhost", 27017)
+  set :mongo_connection, conn
+  set :mongo_db, conn.db('test')
+end
 
 set :port, 3000
 set :bind, '127.0.0.1'
 
 get '/' do
-  craml = Craml.new
-  logger.info "#{craml.to_s}"
+  "Hey ... you've arrive"
+  # craml = Craml.new
+  # logger.info "#{craml.to_s}"
+end
+
+get '/collections/?' do
+  content_type :json
+  settings.mongo_db.collection_names.to_json
 end
 
 post '/payload' do
