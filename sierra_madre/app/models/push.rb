68e45901b5f90763a7a4965644ded0d9731251fc
Zachary Abresch <zachary.abresch@gmail.com>
2015-05-07 18:28:13 -0500

    Extracted method in Push

--- sierra_madre/app/models/push.rb
+++ sierra_madre/app/models/push.rb
@@ -6,9 +6,13 @@ class Push
   end
 
   def has_raml?
-    head_commit = @payload["head_commit"]
-    commit_list = [head_commit["added"], head_commit["modified"]].flatten.compact
     raml_matches = commit_list.grep /\.raml$/
     raml_matches.empty? ? false : raml_matches
   end
+
+  private
+    def commit_list
+      head_commit = @payload["head_commit"]
+      [head_commit["added"], head_commit["modified"]].flatten.compact
+    end
 end
