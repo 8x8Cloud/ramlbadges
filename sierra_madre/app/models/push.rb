d11d21ed130cf847a119b0d351c690cb542571c5
Zachary Abresch <zachary.abresch@gmail.com>
2015-05-09 11:42:25 -0500

    Removed previous tests
    After some design sketching I've decided on a slightly different approach.
    A lot is similar to what I'd started but, in order to drive features with tests,
    I'm starting greenfield again.

--- sierra_madre/app/models/push.rb
+++ sierra_madre/app/models/push.rb
@@ -1,24 +1,2 @@
 class Push
-  attr_accessor :payload
-
-  attr_reader :repository
-
-  def initialize(json = nil)
-    @payload = JSON.parse(json)
-  end
-
-  def repository
-    @repository ||= @payload["repository"]
-  end
-
-  def has_raml?
-    raml_matches = commit_list.grep /\.raml$/
-    raml_matches.empty? ? false : raml_matches
-  end
-
-  private
-    def commit_list
-      head_commit = @payload["head_commit"]
-      [head_commit["added"], head_commit["modified"]].flatten.compact
-    end
 end
--- sierra_madre/spec/models/repository_spec.rb
+++ sierra_madre/spec/models/repository_spec.rb
@@ -1,8 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Repository do
-  it "is an instance of Repository" do
-    push = Repository.new
-    expect(push).to be_instance_of Repository
-  end
 end
